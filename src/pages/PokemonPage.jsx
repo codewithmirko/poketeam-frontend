340797328eb265013d68bbe9bdf3843954c57262
Mirko Schaefer <codewithmirko@gmail.com>
2024-04-17 15:44:41 +0200

    added pagination and fake login

--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -1,62 +1,80 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 import styles from "../styles/PokemonPage.module.css";
 
 const PokemonPage = () => {
-  // Declare a variable to store dynamic data and setter function to update it
   const [pokemonList, setPokemonList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const perPage = 20; // Number of Pokemon to display per page
 
-  // Declare the getAllPokemon function that gets the data from the API
-  const getAllPokemon = async () => {
-    try {
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
-      if (response.ok) {
-        const data = await response.json();
-        const pokemonWithDetails = await Promise.all(
-          data.results.map(async (pokemon) => {
-            const detailResponse = await fetch(pokemon.url);
-            if (detailResponse.ok) {
-              const detailData = await detailResponse.json();
-              return { ...pokemon, details: detailData };
-            } else {
-              throw new Error(`Failed to fetch details for ${pokemon.name}`);
-            }
-          })
-        );
-        setPokemonList(pokemonWithDetails);
-        console.log(pokemonWithDetails);
-      }
-    } catch (error) {
-      console.log("Error fetching the API", error);
-    }
-  };
-
-  // Run useEffect to run a function (the getAllPokemon function) one time at the start
   useEffect(() => {
+    const getAllPokemon = async () => {
+      try {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+        );
+        if (response.ok) {
+          const data = await response.json();
+          const pokemonWithDetails = await Promise.all(
+            data.results.map(async (pokemon) => {
+              const detailResponse = await fetch(pokemon.url);
+              if (detailResponse.ok) {
+                const detailData = await detailResponse.json();
+                return { ...pokemon, details: detailData };
+              } else {
+                throw new Error(`Failed to fetch details for ${pokemon.name}`);
+              }
+            })
+          );
+          setPokemonList(pokemonWithDetails);
+        }
+      } catch (error) {
+        console.log("Error fetching the API", error);
+      }
+    };
     getAllPokemon();
   }, []);
 
-  // Return the actual page below
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const offset = currentPage * perPage;
+  const currentPageData = pokemonList.slice(offset, offset + perPage);
 
   return (
     <>
       <h1>I am the Pokemon Page</h1>{" "}
       <p>Click on the Pokemon Cards if you want to see more details!</p>
-      {/* Loop over the array of objects [{}, {}, ..] and create list elements, names etc for each of the objects */}
-      {pokemonList.map((currentPokemon) => {
+      {currentPageData.map((currentPokemon) => {
         return (
           <div key={currentPokemon.details.id} className={styles.card}>
             <Link to={`/pokemon/${currentPokemon.details.id}`}>
               <img src={currentPokemon.details.sprites.front_default} alt="" />
-              <h1>{currentPokemon.name}</h1>
-              <p>{currentPokemon.details.id}</p>
-              <p>{currentPokemon.details.height}</p>
-              <p>{currentPokemon.details.weight}</p>
+              <h1>
+                {currentPokemon.name[0].toUpperCase() +
+                  currentPokemon.name.slice(1)}
+              </h1>
+              <p>#{currentPokemon.details.id}</p>
+              <p>Height: {currentPokemon.details.height}</p>
+              <p>Weight: {currentPokemon.details.weight}</p>
             </Link>
           </div>
         );
       })}
+      {/* Pagination component */}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        breakLabel={"..."}
+        pageCount={Math.ceil(pokemonList.length / perPage)}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageChange}
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+      />
     </>
   );
 };
